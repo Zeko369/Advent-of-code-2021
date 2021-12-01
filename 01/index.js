ae8b1162e60aacf1abc08331f2d692565e7f5717
Fran Zekan <zekan.fran369@gmail.com>
2021-12-01 06:13:23 +0100

    [01] solutions

--- 01/index.js
+++ 01/index.js
@@ -5,8 +5,32 @@ import { readFile } from "fs/promises";
 (async () => {
   const file = await readFile(join(dirname(fileURLToPath(import.meta.url)), "./input.txt"), "utf8");
   const lines = file.split("\n").map((a) => {
-    return a;
+    return parseInt(a);
   });
 
-  console.log(lines);
+  let a = 0;
+  for (let i = 0; i < lines.length - 1; i++) {
+    if (lines[i] < lines[i + 1]) {
+      a++;
+    }
+  }
+
+  console.log(a);
+
+  let tmp = [];
+  for (let i = 0; i < lines.length - 2; i++) {
+    tmp[i] = 0;
+    for (let j = 0; j < 3; j++) {
+      tmp[i] += lines[i + j];
+    }
+  }
+
+  let b = 0;
+  for (let i = 0; i < tmp.length; i++) {
+    if (tmp[i] < tmp[i + 1]) {
+      b++;
+    }
+  }
+
+  console.log(b);
 })();
