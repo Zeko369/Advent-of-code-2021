9c69e1144aa45c52e83da264acef94d857064ca3
Fran Zekan <zekan.fran369@gmail.com>
2021-12-16 08:07:15 +0100

    [16] - better lib for loading files with

--- 16/index.js
+++ 16/index.js
@@ -1,34 +1,41 @@
 import { loadData } from "../shared/index.js";
 
+const hardCodedData = [
+  ["tmp-0", ["C200B40A82"]],
+  ["tmp-1", ["04005AC33890"]],
+  ["tmp-2", ["880086C3E88112"]],
+  ["tmp-3", ["CE00C43D881120"]],
+  ["tmp-4", ["D8005AC2A8F0"]],
+  ["tmp-5", ["F600BC2D8F"]],
+  ["tmp-6", ["9C005AC2F8F0"]],
+  ["tmp-7", ["9C0141080250320F1802104A08"]],
+];
+
 (async () => {
-  for (const [name, rows] of await loadData(import.meta.url, (row, index) => {
-    return row
-      .split("")
-      .map((a) => parseInt(a, 16).toString(2))
-      .map((a) => {
-        if (a.length < 4) {
-          return "0".repeat(4 - a.length) + a;
-        }
-        return a;
-      })
-      .join("");
-  })) {
-    console.log(`------------- ${name} -------------`);
-    if (name !== "tmp-0") {
-      continue;
+  for (const [name, rows] of await loadData(
+    import.meta.url,
+    (row, index) => {
+      return row
+        .split("")
+        .map((a) => parseInt(a, 16).toString(2))
+        .map((a) => {
+          if (a.length < 4) {
+            return "0".repeat(4 - a.length) + a;
+          }
+          return a;
+        })
+        .join("");
     }
-
-    // if (name > "input5.txt") {
-    //   break;
-    // }
-
-    // if (name !== "input3.txt") {
+    // hardCodedData
+  )) {
+    console.log(`------------- ${name} -------------`);
+    // if (name !== "tmp-0") {
     //   continue;
     // }
 
     const parseHeader = (str) => {
       const version = parseInt(str.slice(0, 3), 2);
-      const id = str.slice(3, 6);
+      const id = parseInt(str.slice(3, 6), 2);
 
       return [version, id];
     };
@@ -73,10 +80,8 @@ import { loadData } from "../shared/index.js";
           }
 
           const [version, id] = parseHeader(rest);
-          console.log(version, id);
-
           cursor += 6;
-          if (id === "100") {
+          if (id === 4) {
             const [num, offset] = parseLiteral(str.slice(cursor));
             i += offset;
             cursor += offset;
@@ -86,26 +91,25 @@ import { loadData } from "../shared/index.js";
             const [operator, c] = parseOperator(str.slice(cursor));
             i += c;
             cursor += c;
-            sub.push({ type: "operator", version, data: operator });
+            sub.push({ type: "operator", op: id, version, data: operator });
           }
         }
       } else {
         // number of sub-packets
         const len = parseInt(str.slice(cursor, cursor + 11), 2);
-        console.log(len);
 
         cursor += 11;
         for (let i = 0; i < len && cursor < str.length; i++) {
           const [version, id] = parseHeader(str.slice(cursor));
           cursor += 6;
-          if (id === "100") {
+          if (id === 4) {
             const [num, offset] = parseLiteral(str.slice(cursor));
             cursor += offset;
             sub.push({ type: "literal", version, data: num });
           } else {
             const [operator, c] = parseOperator(str.slice(cursor));
             cursor += c;
-            sub.push({ type: "operator", version, data: operator });
+            sub.push({ type: "operator", op: id, version, data: operator });
           }
         }
       }
@@ -114,19 +118,21 @@ import { loadData } from "../shared/index.js";
     };
 
     const data = rows[0];
+    // console.log(data);
     console.log(data);
-    if (parseHeader(data)[1] === "100") {
+    if (parseHeader(data)[1] === 4) {
       const [num, offset] = parseLiteral(data.slice(6));
-      console.log(num);
+      console.log("Just literal", num);
     } else {
       const [op] = parseOperator(data.slice(6));
       const final = {
         type: "operator",
+        op: parseHeader(data)[1],
         version: parseHeader(data)[0],
         data: op,
       };
 
-      console.log(JSON.stringify(final, null, 2));
+      // console.log(JSON.stringify(final, null, 2));
 
       const getVersionSum = (foo, v) => {
         if (Array.isArray(foo.data)) {
@@ -144,15 +150,28 @@ import { loadData } from "../shared/index.js";
 
       console.log(getVersionSum(final, 0));
 
-      const evals = {
+      const evalObj = {
         0: (args) => args.reduce((acc, cur) => acc + cur, 0),
         1: (args) => args.reduce((acc, cur) => acc * cur, 1),
-        2: (args) => args.sort((a, b) => a - b)[1],
-        3: (args) => args.sort((a, b) => b - a)[1],
+        2: (args) => args.sort((a, b) => a - b)[0],
+        3: (args) => args.sort((a, b) => b - a)[0],
         5: ([a, b]) => (a > b ? 1 : 0),
         6: ([a, b]) => (a < b ? 1 : 0),
         7: ([a, b]) => (a === b ? 1 : 0),
       };
+
+      const evaluate = (tree, value) => {
+        if (tree.type === "literal") {
+          return tree.data;
+        }
+
+        if (tree.type === "operator") {
+          const args = tree.data.map((a) => evaluate(a, value));
+          return evalObj[tree.op](args);
+        }
+      };
+
+      console.log(evaluate(final, 0));
     }
   }
 })();
--- shared/index.js
+++ shared/index.js
@@ -5,10 +5,15 @@ import { readFile, readdir } from "fs/promises";
 /**
  * @param {string} path
  * @param {(row: string, index: number) => any} mapper
+ * @param {string[][] | undefined} forcedData
  */
-export const loadData = async (path, mapper = (a) => a) => {
-  const files = (await readdir(dirname(fileURLToPath(path)))).filter((file) =>
-    file.endsWith(".txt")
+export const loadData = async (path, mapper = (a) => a, forcedData = undefined) => {
+  if (forcedData) {
+    return forcedData.map(([name, rows]) => [name, rows.map(mapper)]);
+  }
+
+  const files = (await readdir(dirname(fileURLToPath(path)))).filter(
+    (file) => file.endsWith(".txt") && !file.startsWith("_")
   );
 
   return Promise.all(
